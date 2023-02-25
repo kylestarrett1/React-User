4750ecfa8110f2b75c3ca1d433e887b5636e8d22
Kyle Starrett <kylestarrett1@gmail.com>
2023-02-25 13:17:19 -0800

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-import AddUsers from "./components/NewUsers/AddUsers";
+import AddUser from "./components/User/AddUser";
+import UserList from "./components/User/UserList";
 
-function App() {
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          id: Math.random().toString(),
+          username: userName,
+          age: userAge,
+        },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUsers />
+      <AddUser onAddUser={addUserHandler} />
+      {usersList.length !== 0 ? <UserList users={usersList} /> : ""}
     </div>
   );
-}
+};
 
 export default App;
